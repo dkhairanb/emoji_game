08f76a87fbf5f28795ca896b561da9379c7c548e
Dhil <dhil.badjiser@binus.ac.id>
2023-03-21 09:48:27 +0700

    Initial Commit

---
+++ emoji_game/emoji_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_gameApp.swift
+//  emoji_game
+//
+//  Created by Dhil Khairan Badjiser on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct emoji_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
